0e255ddd0f54512c0b3020efc6ce282dbc741fcd
Schuster Alwin (IT OS IC VCP HICP) <alwin.schuster@infineon.com>
2026-03-25 20:50:14 +0100

    #3 Fix JSON errors

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/model/card/Card.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/model/card/Card.kt
@@ -1,14 +1,7 @@
 package at.aau.serg.websocketdemoserver.model.card
 
 import at.aau.serg.websocketdemoserver.model.enums.CardAction
-import com.fasterxml.jackson.annotation.JsonSubTypes
-import com.fasterxml.jackson.annotation.JsonTypeInfo
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "cardType")
-@JsonSubTypes(
-    JsonSubTypes.Type(value = ChanceCard::class,         name = "CHANCE"),
-    JsonSubTypes.Type(value = CommunityChestCard::class, name = "COMMUNITY_CHEST"),
-)
 abstract class Card(
     open val id: Int,
     open val description: String,

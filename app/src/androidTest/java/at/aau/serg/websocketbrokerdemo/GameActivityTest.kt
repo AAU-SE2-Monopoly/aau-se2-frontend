9b77201eecac074952b5e74e66ede66fa56f7e72
davensive <essl.david@gmail.com>
2026-04-02 10:28:01 +0200

    Added more Tests for GameFlow #12

--- app/src/androidTest/java/at/aau/serg/websocketbrokerdemo/GameActivityTest.kt
+++ app/src/androidTest/java/at/aau/serg/websocketbrokerdemo/GameActivityTest.kt
@@ -12,6 +12,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import at.aau.serg.websocketbrokerdemo.networking.Testing.FakeGameService
 import com.example.myapplication.R
+import junit.framework.TestCase.assertEquals
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.CoreMatchers.instanceOf
@@ -23,33 +24,65 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class GameActivityTest {
+
     private lateinit var fakeService: FakeGameService
-    init {
-        ServiceLocator.injectFakeGameService(fakeService)
-    }
+
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(GameActivity::class.java)
-@Test
-fun clickConnectButton_showsConnectingInStatus(){
-    onView(withId(R.id.et_player_name))
-        .perform(typeText("Player1"), closeSoftKeyboard())
-    onView(withId(R.id.btn_connect)).perform(click())
-    onView(withId(R.id.tv_event_log))
-        .check(matches(withText(containsString("connecting"))))
-}
+
+    @Before
+    fun setup() {
+
+        fakeService = FakeGameService()
+        ServiceLocator.injectFakeGameService(fakeService)
+    }
 
     @Test
-        fun test_Create_GameFlow_should_match_with_String_CREATE_GAME() {
+    fun clickConnectButton_showsConnectingInStatus() {
+        onView(withId(R.id.et_player_name))
+            .perform(typeText("Player1"), closeSoftKeyboard())
+        
+        onView(withId(R.id.btn_connect)).perform(click())
+        
+
+        onView(withId(R.id.tv_status))
+            .check(matches(withText(containsString("Connecting"))))
+    }
+
+    @Test
+    fun test_Create_GameFlow_should_match_with_String_CREATE_GAME() {
+        onView(withId(R.id.spinner_action)).perform(click())
+        onData(allOf(instanceOf(String::class.java), `is`("Create Game")))
+            .perform(click())
+
+        onView(withId(R.id.btn_send)).perform(click())
+        
+
+        onView(withId(R.id.tv_event_log))
+            .check(matches(withText(containsString("CREATE_GAME"))))
+    }
+    @Test
+    fun test_Join_GameFlow_should_match_with_String_JOIN_GAME() {
+        onView(withId(R.id.spinner_action)).perform(click())
+        onData(allOf(instanceOf(String::class.java), `is`("Join Game")))
+            .perform(click())
+        onView(withId(R.id.et_game_id))
+            .perform(typeText("gameId"), closeSoftKeyboard())
+        onView(withId(R.id.btn_send)).perform(click())
+
+       onView(withId(R.id.tv_event_log))
+           .check(matches(withText(containsString("JOIN_GAME"))))
+    }
+    @Test
+    fun test_Join_GameFlow_without_GameId_should_not_send_request() {
+        onView(withId(R.id.spinner_action)).perform(click())
+        onData(allOf(instanceOf(String::class.java), `is`("Join Game")))
+            .perform(click())
 
 
-            onView(withId(R.id.spinner_action)).perform(click())
-            onData(allOf(instanceOf(String::class.java), `is`("Create Game")))
-                .perform(click())
-
-            onView(withId(R.id.btn_send)).perform(click())
-            onView(withId(R.id.tv_event_log))
-                .check(matches(withText(containsString("CREATE_GAME"))))
-        }
+        onView(withId(R.id.btn_send)).perform(click())
 
 
+        assertEquals(0, fakeService.joinGameCalls)
+    }
 }
--- app/src/main/java/at/aau/serg/websocketbrokerdemo/networking/Testing/FakeGameService.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/networking/Testing/FakeGameService.kt
@@ -4,57 +4,73 @@ import at.aau.serg.websocketbrokerdemo.networking.GameService
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-/**This Class is only used for testing**/
-class FakeGameService: GameService {
+
+/** This Class is only used for testing **/
+class FakeGameService : GameService {
     private val _events = MutableSharedFlow<String>(replay = 1)
     override val events: SharedFlow<String> = _events.asSharedFlow()
 
     private val _status = MutableSharedFlow<String>(replay = 1)
     override val status: SharedFlow<String> = _status.asSharedFlow()
-    // Fake implementation of GameService
 
     var connectCalled = false
     var lastSubscribedGameId: String? = null
     var lastCreatedPlayerName: String? = null
     var lastJoinedGameId: String? = null
     var lastJoinedPlayerName: String? = null
+    
+    // Call counters
+    var joinGameCalls = 0
+    var createGameCalls = 0
     var startGameCalled = false
     var rollDiceCalled = false
     var endTurnCalled = false
     var requestStateCalled = false
+
     override fun connect() {
-        TODO("Not yet implemented")
+        connectCalled = true
     }
 
     override fun subscribeToGame(gameId: String) {
-        TODO("Not yet implemented")
+        lastSubscribedGameId = gameId
     }
 
     override fun createGame(playerName: String) {
-        TODO("Not yet implemented")
+        createGameCalls++
+        lastCreatedPlayerName = playerName
     }
 
     override fun joinGame(gameId: String, playerName: String) {
-        TODO("Not yet implemented")
+        joinGameCalls++
+        lastJoinedGameId = gameId
+        lastJoinedPlayerName = playerName
     }
 
     override fun startGame() {
-        TODO("Not yet implemented")
+        startGameCalled = true
     }
 
     override fun rollDice() {
-        TODO("Not yet implemented")
+        rollDiceCalled = true
     }
 
     override fun endTurn() {
-        TODO("Not yet implemented")
+        endTurnCalled = true
     }
 
     override fun requestState() {
-        TODO("Not yet implemented")
+        requestStateCalled = true
     }
 
     override fun setGameId(gameId: String) {
-        TODO("Not yet implemented")
+        lastSubscribedGameId = gameId
+    }
+
+    suspend fun emitTestEvent(jsonMessage: String) {
+        _events.emit(jsonMessage)
+    }
+
+    suspend fun emitTestStatus(statusMessage: String) {
+        _status.emit(statusMessage)
     }
 }

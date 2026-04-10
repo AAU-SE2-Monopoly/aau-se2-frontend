5f47dd824127dfad8679c56719e34313fb87338e
joachim185 <jolu.18.05@gmail.com>
2026-04-10 12:51:42 +0200

    test: fix NPE in unit tests by adding org.json dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,13 +153,14 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation(libs.core.ktx)
     testImplementation(libs.equalsverifier)
-
+    testImplementation("org.json:json:20231013")
     // Harte Versionen für Unit Tests (verhindert Konflikte)
     testImplementation("androidx.test.ext:junit:1.1.5")
     testImplementation("androidx.test.espresso:espresso-core:3.5.0")
     testImplementation("androidx.test.espresso:espresso-intents:3.5.0")
 
     // -- ANDROID TESTS (Hier lag der Fehler!) --
+
     // Wir werfen libs.androidx.junit und espresso.core raus und erzwingen die Compose-Versionen
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")

110888be84fea1304881b3e9c6b8adb11a8e8c26
davensive <essl.david@gmail.com>
2026-05-08 23:05:12 +0200

    #61 CI fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
             isReturnDefaultValues = true
             all {
                 it.useJUnitPlatform()
-                it.jvmArgs("-Xss8m")
+                it.jvmArgs("-Xss8m", "-Xmx4g", "-XX:MaxMetaspaceSize=1g")
                 it.testLogging {
                     events("failed", "standardOut", "standardError")
                     exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
